1e68a9afddb33fb0f228cd89fc130ea62eb312c8
Ethan <ethan.gantt@stu.barren.kyschools.us>
2024-03-01 09:45:45 -0600

    Finished Exercise

---
+++ script.js
@@ -0,0 +1,23 @@
+for (i = 1; i < 8; i++){
+    console.log(i);
+  }
+
+  for (i = 5; i <= 25; i += 2){
+    console.log(i);
+  }
+
+  const wizards = [
+    "Harry Potter", 
+    "Hermione Granger", 
+    "Ron Weasley"
+  ]
+
+for (char of wizards){
+    console.log(char);
+}
+
+let harryPotterMovies = 0;
+while (harryPotterMovies != 8){
+    harryPotterMovies = harryPotterMovies + 1
+    console.log(harryPotterMovies);
+}
